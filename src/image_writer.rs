2d37da7490520bfaa4d0b405773e135d26e4cc48
Fletcher Woodruff <fletcherwoodruff@gmail.com>
2023-06-24 08:42:08 -0600

    Add basic PBM image writer
    Adds a class to write out RGB images using PBM. May want to swap this
    for a better image format later but this is nice to start.

---
+++ src/image_writer.rs
@@ -0,0 +1,49 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
+type Pixel = (u8, u8, u8);
+
+pub struct ImageWriter {
+    width: usize,
+    height: usize,
+    image_data: Vec<Pixel>,
+}
+
+impl ImageWriter {
+    pub fn create(width: usize, height: usize) -> Self {
+        let mut image_data = Vec::new();
+        image_data.resize(width * height, (0, 0, 0));
+        Self {
+            width,
+            height,
+            image_data,
+        }
+    }
+
+    pub fn set_pixel(&mut self, row: usize, col: usize, pixel: Pixel) -> () {
+        self.image_data[row * self.width + col] = pixel;
+    }
+
+    pub fn write_image(&self, path: &Path) -> Result<(), std::io::Error> {
+        let mut writer = BufWriter::new(File::create(path)?);
+
+        writer.write("P3".as_bytes())?;
+        writer.write("\n".as_bytes())?;
+        writer.write(self.width.to_string().as_bytes())?;
+        writer.write(" ".as_bytes())?;
+        writer.write(self.height.to_string().as_bytes())?;
+        writer.write("\n".as_bytes())?;
+        writer.write("255".as_bytes())?;
+        writer.write("\n".as_bytes())?;
+        for pixel in &self.image_data {
+            write!(&mut writer, "{} {} {}\n", pixel.0, pixel.1, pixel.2)?;
+        }
+
+        writer.flush()?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+mod image_writer;
 mod ray;
 mod triangle;
 mod v3;
 
-use crate::v3::V3;
+use std::path::Path;
 
-fn main() {
-    let v = V3::create(1.0, 0.0, 0.0);
-    println!("My vec: {:?}", v);
+use image_writer::ImageWriter;
+
+fn main() -> Result<(), std::io::Error> {
+    let mut w = ImageWriter::create(5, 3);
+    w.set_pixel(2, 1, (255, 255, 255));
+    w.write_image(Path::new("./output.pbm"))
 }
